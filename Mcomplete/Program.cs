b6c9085753890cfeaad286d567637be95f29c90f
hyenmingyu <mingyuhyeon6@gmail.com>
2026-09-02 14:49:22 +0900

    gameprojectlibrary

--- Mcomplete/Program.cs
+++ Mcomplete/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace Mcomplete
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello, World!");
+    //    }
+    //}
 }
